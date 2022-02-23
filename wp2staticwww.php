9e25121795e16f1ef88935a23d6c8f47bac7619f
Leon Stafford <leon@strattic.com>
2022-02-23 20:05:41 +1100

    fix naming

--- wp2staticwww.php
+++ wp2staticwww.php
@@ -27,18 +27,18 @@ remove_action( 'wp_head', 'rest_output_link_wp_head' );
 remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 remove_action( 'template_redirect', 'rest_output_link_header', 11 );
 
-function drum_rm_wp_version_number() {
+function wp2staticwwwrm_wp_version_number() {
     return '';
 }
 
-add_filter('the_generator', 'drum_rm_wp_version_number');
+add_filter('the_generator', 'wp2staticwwwrm_wp_version_number');
 
-function drum_remove_wp_block_library_css(){
+function wp2staticwwwremove_wp_block_library_css(){
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'wp-block-library-theme' );
     wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
 }
 
-add_action( 'wp_enqueue_scripts', 'drum_remove_wp_block_library_css', 100 );
+add_action( 'wp_enqueue_scripts', 'wp2staticwwwremove_wp_block_library_css', 100 );
 
 // test comment
